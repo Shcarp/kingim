5d9f669ae780d81c1f2ed597e131b6997e3a892b
sh-2020 <2191523412@qq.com>
2021-09-12 17:03:36 +0800

    11

--- examples/kimbench/cmd.go
+++ examples/kimbench/cmd.go
@@ -23,7 +23,7 @@ func NewBenchmarkCmd(ctx context.Context) *cobra.Command {
 		Short: "kingim benchmark tools",
 	}
 	var opts = &Options{}
-	cmd.PersistentFlags().StringVarP(&opts.Addr, "address", "a", "ws://124.71.204.19:8000", "server address")
+	cmd.PersistentFlags().StringVarP(&opts.Addr, "address", "a", "ws://1.117.56.86:8000", "server address")
 	cmd.PersistentFlags().StringVarP(&opts.AppSecret, "appSecret", "s", token.DefaultSecret, "app secret")
 	cmd.PersistentFlags().IntVarP(&opts.Count, "count", "c", 100, "request count")
 	cmd.PersistentFlags().IntVarP(&opts.Threads, "thread", "t", 10, "thread count")
